2d350f2accd213d71801d2bab136ecba2d546c4e
kuoliang018 <32524841+kuoliang018@users.noreply.github.com>
2019-04-25 14:16:34 -0700

    for pub

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const server = app.listen(8080, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
 
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs'); // sorry i did not we use ejs anywhere not sure if this is needed
+app.use(express.static(__dirname + '/public')); // This line.
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
