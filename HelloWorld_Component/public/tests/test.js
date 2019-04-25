66d5881204d48be06000a9401551edfd56474b1b
Sarah Gemperle <sarahgemp15@gmail.com>
2019-04-25 16:04:07 -0700

    edited test to fail should not push

--- HelloWorld_Component/public/tests/test.js
+++ HelloWorld_Component/public/tests/test.js
@@ -6,5 +6,13 @@ const server = require('../../server');
 describe('test', () => {
   it('should return a string', () => {
     expect('ci with travis').to.equal('ci with travis');
+    console.log("test worked!");
   });
 });
+
+describe('test', () => {
+    it('should return a string', () => {
+      expect('ci with travis').to.equal('ci with tra');
+      console.log("test worked!");
+    });
+  });
