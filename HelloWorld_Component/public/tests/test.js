24bebf0ae4b5caf82a4ff9aebc1d409b6def36f7
Sarah Gemperle <sarahgemp15@gmail.com>
2019-04-28 18:21:16 -0700

    added standard to test in package.json

--- HelloWorld_Component/public/tests/test.js
+++ HelloWorld_Component/public/tests/test.js
@@ -1,18 +1,18 @@
 // index.spec.js
 
 const expect = require('chai').expect
-const server = require('../../server');
+const server = require('../../server')
 
-describe('test', function() {
-  it('should return a string', function() {
-    expect('ci with travis').to.equal('ci with travis');
-    console.log("test worked!");
-  });
-});
+describe('test', function () {
+  it('should return a string', function () {
+    expect('ci with travis').to.equal('ci with travis')
+    console.log('test worked!')
+  })
+})
 
 /* describe('test', () => {
     it('should return a string', () => {
       expect('ci with travis').to.equal('ci with tra');
       console.log("test worked!");
     });
-  }); */ 
+  }); */
