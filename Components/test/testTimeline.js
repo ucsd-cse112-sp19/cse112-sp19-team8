071f45974d1ee78d7daf703296984fad2a315627
Joshua Quan <JMQ0598@github.com>
2019-06-09 05:57:33 -0700

    timeline tests

---
+++ Components/test/testTimeline.js
@@ -0,0 +1,23 @@
+describe('Testing component Timeline', () => {
+  var component
+
+  beforeEach(function () {
+    component = new Timeline()
+  })
+
+  describe('Test Reverse', () => {
+    it('should enable attribute reverse upon property changes', () => {
+      component.reverse = true
+      expect(component.getAttribute('reverse')).equal(true)
+      expect(component.shadowRoot.querySelector('.main')
+        .style.flexDirection).equal('column-reverse')
+    })
+
+    it('should enable attribute reverse upon property changes', () => {
+      component.reverse = false
+      expect(component.getAttribute('reverse')).equal(false)
+      expect(component.shadowRoot.querySelector('.main')
+        .style.flexDirection).equal('column')
+    })
+  })
+})
