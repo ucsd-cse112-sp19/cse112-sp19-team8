c6af5fcd2f24af62ddc09c642b0071156f2f3f37
Joel Loo <j2loo97@gmail.com>
2019-06-09 15:50:07 -0700

    fixing initial tests, updated some grammar on teamsite demo

--- Components/test/cafe.js
+++ Components/test/cafe.js
@@ -1,22 +1,18 @@
 import { Selector } from 'testcafe'; // first import testcafe selectors
- 
-fixture `Getting Started`// declare the fixture
+
+fixture `--- Component Tests ---`// declare the fixture
     .page `../public/componentDemo.html`;  // specify the start page
- 
- 
-//then create a test and place your code there
-test('My first test', async t => {
+
+// Expect carousel's starting interval to be 1700
+test('Carousel Initial Test', async t => {
     await t
         .click('#itv-plus')
- 
-        // Use the assertion to check if the actual header text is equal to the expected one
         .expect(Selector('#carousel').getAttribute('interval')).eql('1700');
 });
 
-test('My first test', async t => {
+// Expect timeline's starting child node count to be 4
+test('Timeline Initial Test', async t => {
     await t
         .click('#itv-plus')
- 
-        // Use the assertion to check if the actual header text is equal to the expected one
         .expect(Selector('#timeline').childNodeCount).eql(4);
 });
